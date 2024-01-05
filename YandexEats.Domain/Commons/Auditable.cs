78afe4e73de83291bdccd5b2d29309ed4b2aa805
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-05 21:33:29 +0500

    Domain

---
+++ YandexEats.Domain/Commons/Auditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexEats.Domain.Commons
+{
+    public abstract class Auditable
+    {
+        public int Id { get; set; }
+
+        public string IsCreated { get; set; } = DateTime.Now.ToString();
+    }
+}
---
+++ YandexEats.Domain/Entities/Users/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexEats.Domain.Commons;
+using YandexEats.Domain.Entities.Foods;
+
+namespace YandexEats.Domain.Entities.Users
+{
+    public class Users : Auditable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Addres { get; set; }
+
+        public Food Food { get; set; }
+    }
+}
