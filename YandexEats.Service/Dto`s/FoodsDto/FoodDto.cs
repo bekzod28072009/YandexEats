12c54c65c0c97fad2a386464533a6af39b6f8c1a
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-09 12:37:35 +0500

    Dto

---
+++ YandexEats.Service/Dto`s/FoodsDto/FoodDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexEats.Service.Dto_s.FoodsDto
+{
+    public class FoodDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Payment { get; set; }
+    }
+}
---
+++ YandexEats.Service/Dto`s/UsersDto/UsersDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexEats.Domain.Entities.Foods;
+using YandexEats.Service.Dto_s.FoodsDto;
+
+namespace YandexEats.Service.Dto_s.UsersDto
+{
+    public class UsersDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Addres { get; set; }
+
+        public FoodDto Food { get; set; }
+    }
+}
