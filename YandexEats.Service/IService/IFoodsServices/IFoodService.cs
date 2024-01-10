6376d424c9d726aa064bda9089420eb15924ef58
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-01-10 15:00:11 +0500

    Interface Service

---
+++ YandexEats.Service/IService/IFoodsServices/IFoodService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using YandexEats.Domain.Entities.Foods;
+using YandexEats.Service.Dto_s.FoodsDto;
+
+namespace YandexEats.Service.IService.IFoodsServices
+{
+    public interface IFoodService
+    {
+        IQueryable<Food> GetAll(Expression<Func<Food, bool>> expression, string[] includes = null);
+        ValueTask<FoodDto> GetAsync(Expression<Func<Food, bool>> expression, string[] includes = null);
+        ValueTask<FoodDto> CreateAsync(FoodDto entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<Food, bool>> expression);
+        FoodDto Update(int id, FoodDto entity);
+    }
+}
